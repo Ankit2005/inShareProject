db2e38269e50e503e4a0b61386fcaa9c7c61c073
Ankit2005 <ankitmb15@gmail.com>
2021-06-11 07:27:09 +0530

    add scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,31 +1,35 @@
 const File = require('./models/file.js');
+const schedule = require('node-schedule');
 const fs = require('fs');
 const connectDB = require('./config/db');
 connectDB();
 
 async function deletedOldFiles() {
+   const job = schedule.scheduleJob('1 * * * * *', async function () {
+        console.log('3434')
+        const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
+        
+        //const oldFiles = await File.find({ size: { $lt: '2479720' } });
+        const oldFiles = await File.find({ createdAt: { $lt: pastDate } });
 
-    const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000);
-    console.log(pastDate)
-
-    const oldFiles = await File.find({ createdAt: { $lt: pastDate } });
-
-    // console.log('old files list')
-    // console.log(oldFiles)
-    if (oldFiles.length) {
-        for (const file of oldFiles) {
-            try {
-                fs.unlinkSync(file.path);
-                await file.remove();
-                console.log(`successfully deleted ${file.filename}`);
-            } catch(err) {
-                console.log(`error while deleting file ${err} `);
-            }
+        console.log('oldFiles');
+        console.log(oldFiles);
+        if (oldFiles.length) {
+            for (const file of oldFiles) {
+                try {
+                    fs.unlinkSync(file.path);
+                    await file.remove();
+                    console.log(`successfully deleted ${file.filename}`);
+                } catch (err) {
+                    console.log(`error while deleting file ${err} `);
+                }
+            }            
         }
+    });
 
-        console.log('jon done .');
-
-    }
+    job()
+    console.log('jon done .');
 }
 
+// deletedOldFiles();
 deletedOldFiles().then(process.exit);
--- services/emailService.js
+++ services/emailService.js
@@ -1,26 +1,54 @@
 
 const nodemailer = require("nodemailer");
+
 module.exports = async ({ from, to, subject, text, html }) => {
-    let transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: false, // true for 465, false for other ports
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
         auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS // generated ethereal password
-        },
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: `inShare <${from}>`, // sender address
-        to: to, // list of receivers
-        subject: subject, // Subject line
-        text: text, // plain text body
-        html: html, // html body
-    });
-
-    console.log('########### ########### ############');
-    console.log(info)
+          user: 'ankitmb15@gmail.com', 
+          pass: 'ankit2@@5.'
+        }
+      });
+      
+      var mailOptions = {
+        from,
+        to,
+        subject,
+        text,
+        html
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
 }
 
+
+
+// module.exports = async ({ from, to, subject, text, html }) => {
+//     let transporter = nodemailer.createTransport({
+//         host: process.env.SMTP_HOST,
+//         port: process.env.SMTP_PORT,
+//         secure: false, // true for 465, false for other ports
+//         auth: {
+//             user: process.env.MAIL_USER,
+//             pass: process.env.MAIL_PASS // generated ethereal password
+//         },
+//     });
+
+//     // send mail with defined transport object
+//     let info = await transporter.sendMail({
+//         from: `inShare <${from}>`, // sender address
+//         to: to, // list of receivers
+//         subject: subject, // Subject line
+//         text: text, // plain text body
+//         html: html, // html body
+//     });
+
+//     console.log('########### ########### ############');
+//     console.log(info)
+// }
